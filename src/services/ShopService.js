5aa8de12bed426eb5352c3e9bd126887c5fbcc4e
vladwithcode <vladwithcode@gmail.com>
2022-05-24 17:02:59 -0500

    Shop Service

---
+++ src/services/ShopService.js
@@ -0,0 +1,23 @@
+import makeServerRequest from '../utils/makeServerRequest';
+
+class ShopService {
+  async fetchProducts({ ctg, size, limit = 8, skip = 0 }) {
+    const params = new URLSearchParams();
+
+    if (ctg?.length > 0) params.append('ctg', ctg);
+    if (size?.length > 0) params.append('size', size);
+
+    params.append('limit', limit);
+    params.append('skip', skip);
+
+    const res = await makeServerRequest(
+      '/public/products?'.concat(params.toString())
+    );
+
+    return res;
+  }
+
+  async getFilterOptions() {}
+}
+
+export default new ShopService();
