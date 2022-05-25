65f06c04ca8bcffab3833dc71f74d2408fd6c54d
vladwithcode <vladwithcode@gmail.com>
2022-05-25 12:08:08 -0500

    Summary files

---
+++ src/components/Summary/Summary.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Summary() {
+  return (
+    <div className='summary'>
+      <h3 className='summary__title'>Resumen</h3>
+    </div>
+  );
+}
+
+export default Summary;
