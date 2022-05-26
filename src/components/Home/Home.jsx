53eb3e49ae29bfae42773f529c5f79eebd12c8f0
vladwithcode <vladwithcode@gmail.com>
2022-05-26 12:08:44 -0500

    Init Cart on Homepage load

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import CartContext from '../../context/Cart/CartContext';
+import CartService from '../../services/CartService';
 import Menu from '../Menu/Menu';
 import Modal from '../Modal/Modal';
 import Summary from '../Summary/Summary';
 
 function Home() {
+  const { setCart } = useContext(CartContext);
+
+  useEffect(() => {
+    const initCart = async () => {
+      const cart = await CartService.initCart(false);
+
+      setCart(cart);
+    };
+
+    initCart();
+  }, []);
+
   return (
     <>
       <Menu />
