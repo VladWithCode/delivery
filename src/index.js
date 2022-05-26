ede06a4badfdd0dbbf37c8cc10a81e066c1e45e2
vladwithcode <vladwithcode@gmail.com>
2022-05-26 14:02:28 -0500

    Order Provider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CartProvider } from './context/Cart/CartProvider';
 import { ModalProvider } from './context/Modal/ModalProvider';
+import OrderProvider from './context/Order/OrderProvider';
 import { ToastProvider } from './context/Toast/ToastProvider';
 import AppRouter from './routers/AppRouter';
 import './sass/main.scss';
@@ -13,7 +14,9 @@ root.render(
     <ToastProvider>
       <ModalProvider>
         <CartProvider>
-          <AppRouter />
+          <OrderProvider>
+            <AppRouter />
+          </OrderProvider>
         </CartProvider>
       </ModalProvider>
     </ToastProvider>
