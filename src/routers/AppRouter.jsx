1aaa4f68a6d1100544c6883353abcebf0e8db7a4
vladwithcode <vladwithcode@gmail.com>
2022-05-26 13:58:48 -0500

    OrderProvider

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from '../App';
+import Checkout from '../components/Checkout/Checkout';
 import Home from '../components/Home/Home';
 import NotFound from '../components/NotFound/NotFound';
 
@@ -10,6 +11,7 @@ function AppRouter() {
       <Routes>
         <Route path='/' element={<App />}>
           <Route index element={<Home />} />
+          <Route path='/checkout' element={<Checkout />} />
           <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
