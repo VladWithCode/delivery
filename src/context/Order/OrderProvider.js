1389d229ac8d6f0c518b2b99f62ad55af1d915f3
vladwithcode <vladwithcode@gmail.com>
2022-05-27 13:55:33 -0500

    PaymentInfo fields & methods

--- src/context/Order/OrderProvider.js
+++ src/context/Order/OrderProvider.js
@@ -1,7 +1,14 @@
 import React, { useReducer } from 'react';
-import ToastContext from '../Toast/ToastContext';
+import OrderContext from '../Order/OrderContext';
 import OrderReducer from './OrderReducer';
-import { SET_CUSTOMER_INFO, SET_PAYMENT_INFO } from './types';
+import {
+  SET_CHECKOUT_STEP,
+  SET_CUSTOMER_INFO,
+  SET_PAYMENT_INFO,
+  SET_PAYMENT_METHOD,
+  SET_STRIPE_CLIENT_SECRET,
+  SET_STRIPE_INTENT_ID,
+} from './types';
 
 function OrderProvider({ children }) {
   const initialState = {
@@ -11,7 +18,13 @@ function OrderProvider({ children }) {
       zip: '',
       phone: '',
     },
-    paymentInfo: {},
+    paymentInfo: {
+      method: 'cash',
+      stripeIntentId: '',
+      stripeClientSecret: '',
+      saleId: '',
+    },
+    checkoutStep: 'CUSTOMER_INFO', // CUSTOMER_INFO | PAYMENT_METHOD_SELECTION | COMPLETE_PAYMENT | ORDER_DETAILS
   };
 
   const [state, dispatch] = useReducer(OrderReducer, initialState);
@@ -24,16 +37,45 @@ function OrderProvider({ children }) {
     dispatch({ type: SET_PAYMENT_INFO, payload: info });
   };
 
+  /**
+   *
+   * @param {'card' | 'cash'} method - The payment method choosed by the user
+   */
+  const setPaymentMethod = method => {
+    dispatch({ type: SET_PAYMENT_METHOD, payload: method });
+  };
+
+  const setStripeIntentId = id => {
+    dispatch({ type: SET_STRIPE_INTENT_ID, payload: id });
+  };
+
+  const setStripeClientSecret = secret => {
+    dispatch({ type: SET_STRIPE_CLIENT_SECRET, payload: secret });
+  };
+
+  /**
+   *
+   * @param {'CUSTOMER_INFO' | 'PAYMENT_METHOD_SELECTION' | 'COMPLETE_PAYMENT' | 'ORDER_DETAILS'} step -
+   */
+  const setCheckoutStep = step => {
+    dispatch({ type: SET_CHECKOUT_STEP, payload: step });
+  };
+
   return (
-    <ToastContext.Provider
+    <OrderContext.Provider
       value={{
         customerInfo: state.customerInfo,
         paymentInfo: state.paymentInfo,
+        checkoutStep: state.checkoutStep,
         setCustomerInfo,
         setPaymentInfo,
+        setCheckoutStep,
+        setPaymentMethod,
+        setStripeIntentId,
+        setStripeClientSecret,
       }}>
       {children}
-    </ToastContext.Provider>
+    </OrderContext.Provider>
   );
 }
 
--- src/context/Order/OrderReducer.js
+++ src/context/Order/OrderReducer.js
@@ -1,4 +1,11 @@
-import { SET_CUSTOMER_INFO, SET_PAYMENT_INFO } from './types';
+import {
+  SET_CHECKOUT_STEP,
+  SET_CUSTOMER_INFO,
+  SET_PAYMENT_INFO,
+  SET_PAYMENT_METHOD,
+  SET_STRIPE_CLIENT_SECRET,
+  SET_STRIPE_INTENT_ID,
+} from './types';
 
 const OrderReducer = (state, action) => {
   const { type, payload } = action;
@@ -9,6 +16,26 @@ const OrderReducer = (state, action) => {
 
     case SET_PAYMENT_INFO:
       return { ...state, paymentInfo: payload };
+
+    case SET_CHECKOUT_STEP:
+      return { ...state, checkoutStep: payload };
+
+    case SET_PAYMENT_METHOD:
+      return {
+        ...state,
+        paymentInfo: { ...state.paymentInfo, method: payload },
+      };
+    case SET_STRIPE_CLIENT_SECRET:
+      return {
+        ...state,
+        paymentInfo: { ...state.paymentInfo, stripeClientSecret: payload },
+      };
+
+    case SET_STRIPE_INTENT_ID:
+      return {
+        ...state,
+        paymentInfo: { ...state.paymentInfo, stripeIntentId: payload },
+      };
 
     default:
       return state;
