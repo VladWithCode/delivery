d6d365cf018ea13b36d7f983b134a297e4546c47
vladwithcode <vladwithcode@gmail.com>
2022-05-27 13:49:24 -0500

    stripe public key (TEST)

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,3 +1,6 @@
 export const SERVER_URL = 'http://192.168.1.129:3030';
 
 export const API_URL = SERVER_URL + '/api';
+
+export const STRIPE_PUBLIC_KEY =
+  'pk_test_51L45U5JGXk3T3M1JtUhSae4B9Mp5UIsBfaIopfoxjDKs3ilaauIuwNHY0GrMFHoPEPyw3V8pZ9hRY1v5w0R6H8ZY00JcgMcQOK';
