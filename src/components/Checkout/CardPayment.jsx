8b0174474f4165e69c4cbcaeb74ff28b2e769c3a
vladwithcode <vladwithcode@gmail.com>
2022-05-28 11:54:18 -0500

    Card Payment Component

--- src/components/Checkout/CardPayment.jsx
+++ src/components/Checkout/CardPayment.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react';
 import OrderContext from '../../context/Order/OrderContext';
+import Stripe from '../Stripe/Stripe';
 
 function CardPayment() {
   const {} = useContext(OrderContext);
 
-  return <div className='card-payment'></div>;
+  return (
+    <div className='card-payment'>
+      <Stripe />
+    </div>
+  );
 }
 
 export default CardPayment;
