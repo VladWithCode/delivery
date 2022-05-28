d5055b58280bd256a0a80c9fc700699809e3018a
vladwithcode <vladwithcode@gmail.com>
2022-05-28 11:51:44 -0500

    No stripe sk on public file

--- api/config/env.js
+++ api/config/env.js
@@ -18,8 +18,6 @@ env.SESSION_SECRET = process.env.SESSION_SECRET || 'supersecretsecret';
 
 env.COOKIE_SECRET = process.env.COOKIE_SECRET || 'keyboardcatcookies';
 
-env.STRIPE_SK =
-  process.env.STRIPE_SK ||
-  'sk_test_51L45U5JGXk3T3M1Je5jCAQRpntus6uT8JP65PJSpOPdbXYhNocQ0FXpycxhraNF5EYFKUpJpGuZj5NFCyVhJjLpZ00zxh2G5Y8';
+env.STRIPE_SK = process.env.STRIPE_SK;
 
 module.exports = env;
