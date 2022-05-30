d59279cba62311f5d725c0d3c229dc06e8295370
vladwithcode <vladwithcode@gmail.com>
2022-05-30 10:35:49 -0500

    Init Cart on App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useContext, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from './components/Layout/Footer';
 import Header from './components/Layout/Header';
 import Toast from './components/Toast/Toast';
+import CartContext from './context/Cart/CartContext';
+import CartService from './services/CartService';
 
 function App() {
+  const { setCart } = useContext(CartContext);
+  const [cartInitialized, setCartInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!cartInitialized) {
+      const initCart = async () => {
+        const cart = await CartService.initCart(false);
+
+        setCart(cart);
+        setCartInitialized(true);
+      };
+
+      initCart();
+    }
+  }, [cartInitialized]);
+
   return (
     <>
       <Header />
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,9 @@
 import React, { useContext, useEffect } from 'react';
-import CartContext from '../../context/Cart/CartContext';
-import CartService from '../../services/CartService';
 import Menu from '../Menu/Menu';
 import Modal from '../Modal/Modal';
 import Summary from '../Summary/Summary';
 
 function Home() {
-  const { setCart } = useContext(CartContext);
-
-  useEffect(() => {
-    const initCart = async () => {
-      const cart = await CartService.initCart(false);
-
-      setCart(cart);
-    };
-
-    initCart();
-  }, []);
-
   return (
     <>
       <Menu />
