f03d23b066f50cc482187f77a4b0283cc8e9a213
vladwithcode <vladwithcode@gmail.com>
2022-05-30 10:35:18 -0500

    Payment Steps

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -5,27 +5,28 @@ import CustomerInfo from './CustomerInfo';
 import PaymentMethodSelect from './PaymentMethodSelect';
 
 function Checkout() {
-  const {
-    checkoutStep,
-    paymentInfo: { method },
-  } = useContext(OrderContext);
+  const { checkoutStep } = useContext(OrderContext);
   const [stepTitle, setStepTitle] = useState('');
   const [stepSubtitle, setStepSubtitle] = useState('');
   const [stepElement, setStepElement] = useState(null);
 
   useEffect(() => {
     switch (checkoutStep) {
-      case 'PAYMENT_METHOD_SELECTION':
+      case 'PAYMENT_METHOD_SELECTION': // 2
         setStepTitle('Metodo de pago');
         setStepSubtitle('Seleccione un metodo de pago');
         setStepElement(<PaymentMethodSelect />);
         break;
-      case 'COMPLETE_PAYMENT':
-        setStepTitle('Completar Pago');
+      case 'CARD_PAYMENT': // 3
+        setStepTitle('Pago con tarjeta');
         setStepSubtitle('Ingrese los datos de su tarjeta de credito/debito');
         setStepElement(<CardPayment />);
         break;
-      case 'CUSTOMER_INFO':
+      case 'CONFIRM_ORDER': // 4
+        break;
+      case 'POST_PAYMENT': // 5
+        break;
+      case 'CUSTOMER_INFO': // 1
       default:
         setStepTitle('Datos Generales');
         setStepSubtitle('Ingrese su información y datos de entrega');
