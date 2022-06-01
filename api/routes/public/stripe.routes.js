a8440f6137a5c1d536d9be2404daa703573faf79
vladwithcode <vladwithcode@gmail.com>
2022-06-01 14:54:48 -0500

    Save Stripe Sale endpoint

--- api/routes/public/stripe.routes.js
+++ api/routes/public/stripe.routes.js
@@ -1,9 +1,14 @@
-const { createPaymentIntent } = require('../../controllers/stripe.ctrl');
+const {
+  createPaymentIntent,
+  saveSale,
+} = require('../../controllers/stripe.ctrl');
 
 const Router = require('express').Router;
 
 const router = Router();
 
 router.post('/payment-intent', createPaymentIntent);
+
+router.post('/sale', saveSale);
 
 module.exports = router;
