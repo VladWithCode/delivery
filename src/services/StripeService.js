5af3f159d756f0622f30cb84085a46e3c2911e99
vladwithcode <vladwithcode@gmail.com>
2022-06-01 15:21:32 -0500

    saveToDb method

--- src/services/StripeService.js
+++ src/services/StripeService.js
@@ -11,7 +11,29 @@ class StripeService {
     return res;
   }
 
-  async saveSaleToDB(intentId) {}
+  async saveSaleToDB(intentId, saleData) {
+    if (!saleData || saleData.cart?.items.length === 0)
+      return { message: 'La orden no puede estar vacia' };
+
+    const reqBody = {
+      cart: saleData.cart,
+      customer: {
+        name: saleData.customer.name,
+        phone: saleData.customer.phone,
+      },
+      address: saleData.customer.address,
+      zip: saleData.customer.zip,
+      intentId,
+    };
+
+    const res = await makeServerRequest('/public/stripe/sale', {
+      method: 'POST',
+      body: reqBody,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    return res;
+  }
 }
 
 export default new StripeService();
