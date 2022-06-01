5c748dfecabcbc734fbe8eb5837a13d891ddb7e3
vladwithcode <vladwithcode@gmail.com>
2022-06-01 14:55:14 -0500

    public Sale endpoints

--- api/routes/public.routes.js
+++ api/routes/public.routes.js
@@ -2,11 +2,14 @@ const Router = require('express').Router;
 
 const productRoutes = require('./public/product.routes');
 const stripeRoutes = require('./public/stripe.routes');
+const saleRoutes = require('./public/sale.routes');
 
 const router = Router();
 
 router.use('/products', productRoutes);
 
 router.use('/stripe', stripeRoutes);
+
+router.use('/sales', saleRoutes);
 
 module.exports = router;
