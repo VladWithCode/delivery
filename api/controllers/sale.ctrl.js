22382a6d62f7d3f18d69b0ead27af9daaf722746
vladwithcode <vladwithcode@gmail.com>
2022-06-01 14:53:10 -0500

    Sale Controller

---
+++ api/controllers/sale.ctrl.js
@@ -0,0 +1,25 @@
+const asyncHandler = require('../utils/asyncHandler');
+const saleService = require('../services/sale.service');
+
+const ctrl = {};
+
+ctrl.getSale = async (req, res, next) => {
+  const { id } = req.params;
+
+  const [sale, findError] = await asyncHandler(saleService.getSale(id));
+
+  if (findError) return next(findError);
+
+  if (!sale)
+    return res.json({
+      status: 'NOT_FOUND',
+      message: 'No se encontro venta con el id proporcionado',
+    });
+
+  return res.json({
+    status: 'OK',
+    sale,
+  });
+};
+
+module.exports = ctrl;
