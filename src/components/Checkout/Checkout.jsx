5674167d4b88c60871298ac7d11340fbf5f996f2
vladwithcode <vladwithcode@gmail.com>
2022-06-01 15:11:31 -0500

    Payment Steps

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import OrderContext from '../../context/Order/OrderContext';
 import CardPayment from './CardPayment';
+import CashPayment from './CashPayment';
+import Confirm from './Confirm';
 import CustomerInfo from './CustomerInfo';
 import PaymentMethodSelect from './PaymentMethodSelect';
 
@@ -12,20 +14,34 @@ function Checkout() {
 
   useEffect(() => {
     switch (checkoutStep) {
-      case 'PAYMENT_METHOD_SELECTION': // 2
+      case 'CONFIRM_ORDER': // 2
+        setStepTitle('Confirmar Orden');
+        setStepSubtitle(
+          'Revise y confirme que los contenidos de su orden sean correctos'
+        );
+        setStepElement(<Confirm />);
+        break;
+
+      case 'PAYMENT_METHOD_SELECTION': // 3
         setStepTitle('Metodo de pago');
         setStepSubtitle('Seleccione un metodo de pago');
         setStepElement(<PaymentMethodSelect />);
         break;
-      case 'CARD_PAYMENT': // 3
+
+      case 'CARD_PAYMENT': // 4
         setStepTitle('Pago con tarjeta');
         setStepSubtitle('Ingrese los datos de su tarjeta de credito/debito');
         setStepElement(<CardPayment />);
         break;
-      case 'CONFIRM_ORDER': // 4
+      case 'CASH_PAYMENT': // 4
+        setStepTitle('Pago en efectivo');
+        setStepSubtitle('Confirme que desea realizar el pago en efectivo.');
+        setStepElement(<CashPayment />);
         break;
+
       case 'POST_PAYMENT': // 5
         break;
+
       case 'CUSTOMER_INFO': // 1
       default:
         setStepTitle('Datos Generales');
