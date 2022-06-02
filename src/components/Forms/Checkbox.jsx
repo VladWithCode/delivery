b38355beaf27bbb07789f984f5947462cb3d3811
vladwithcode <vladwithcode@gmail.com>
2022-06-02 14:10:24 -0500

    Checkbox Component

---
+++ src/components/Forms/Checkbox.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Checkbox({ isChecked, onChange, name, label, classNames = {} }) {
+  const handleChange = ({ target }) => {
+    onChange(target.name, target.checked);
+  };
+
+  return (
+    <div
+      className={'form__group--check'.concat(
+        classNames.wrapper ? ' ' + classNames.wrapper : ''
+      )}>
+      <div className='check-wrap'>
+        <input
+          type='checkbox'
+          name={name}
+          id={name}
+          className={'form__check'.concat(
+            classNames.input ? ' ' + classNames.input : ''
+          )}
+          checked={isChecked}
+          onChange={handleChange}
+        />
+      </div>
+      <p className='form__paraph'>
+        <label
+          htmlFor={name}
+          className={'form__label'.concat(
+            classNames.label ? ' ' + classNames.label : ''
+          )}>
+          {label}
+        </label>
+      </p>
+    </div>
+  );
+}
+
+export default Checkbox;
