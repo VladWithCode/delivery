e0ee847473705190ac9c3396f12858b097fb9bfb
vladwithcode <vladwithcode@gmail.com>
2022-06-02 14:10:59 -0500

    Moved Sidebar Toggler

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Header from './components/Layout/Header';
 import Sidebar from './components/Layout/Sidebar';
 import Toast from './components/Toast/Toast';
 import CartContext from './context/Cart/CartContext';
+import SidebarContext from './context/Sidebar/SidebarContext';
 import CartService from './services/CartService';
 
 function App() {
+  const { isActive, setIsActive } = useContext(SidebarContext);
   const { setCart } = useContext(CartContext);
   const [cartInitialized, setCartInitialized] = useState(false);
 
@@ -26,6 +28,15 @@ function App() {
   return (
     <>
       <Header />
+
+      <div
+        className={'header__toggler'.concat(isActive ? ' active' : '')}
+        onClick={() => setIsActive(!isActive)}>
+        <span className='bar'></span>
+        <span className='bar'></span>
+        <span className='bar'></span>
+      </div>
+
       <Outlet />
       <Sidebar />
       {/* <Footer /> */}
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,22 +1,12 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import SidebarContext from '../../context/Sidebar/SidebarContext';
 
 function Header() {
-  const { isActive, setIsActive } = useContext(SidebarContext);
-
   return (
     <header className='header'>
       <h2 className='header__heading'>
         <Link to='/'>POLLO</Link>
       </h2>
-      <div
-        className={'header__toggler'.concat(isActive ? ' active' : '')}
-        onClick={() => setIsActive(!isActive)}>
-        <span className='bar'></span>
-        <span className='bar'></span>
-        <span className='bar'></span>
-      </div>
     </header>
   );
 }
