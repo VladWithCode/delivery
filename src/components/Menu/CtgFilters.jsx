23121264b5b0dc5ee707d9c6ba20da66b8c20e81
vladwithcode <vladwithcode@gmail.com>
2022-06-04 12:21:17 -0500

    Get categories from menu & checkbox handler

--- src/components/Menu/CtgFilters.jsx
+++ src/components/Menu/CtgFilters.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import MenuContext from '../../context/Menu/MenuContext';
 import Checkbox from '../Forms/Checkbox';
+import CtgCheckbox from './CtgCheckbox';
 import Filter from './Filter';
 
 function CtgFilters() {
+  const { menu, ctgs, setActiveCategories } = useContext(MenuContext);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    let ctgs = [];
+
+    for (const ctg in menu) {
+      ctgs.push(ctg);
+    }
+
+    setCategories(ctgs);
+  }, [menu]);
+
+  const handleCheckboxClick = (checked, category) => {
+    if (!checked) {
+      setActiveCategories(ctgs.filter(ctg => ctg !== category));
+      return;
+    }
+
+    if (!ctgs.includes(category)) {
+      setActiveCategories([...ctgs, category]);
+    }
+  };
+
   return (
     <Filter title='Por Categoria'>
-      <Checkbox
-        isChecked={false}
-        name='filter-cat-asada'
-        label='Asada'
-        classNames={{
-          wrapper: 'card__row',
-          input: 'filter__input',
-          label: 'filter__label fs-4',
-        }}
-      />
-      <Checkbox
-        isChecked={false}
-        name='filter-cat-pastor'
-        label='Pastor'
-        classNames={{
-          wrapper: 'card__row',
-          input: 'filter__input',
-          label: 'filter__label fs-4',
-        }}
-      />
+      {categories.map(ctg => (
+        <CtgCheckbox
+          ctg={ctg}
+          active={ctgs.includes(ctg)}
+          handleClick={handleCheckboxClick}
+          key={ctg}
+        />
+      ))}
     </Filter>
   );
 }
