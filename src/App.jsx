bf099858417a63f66de4441b220223a3235cb6fe
vladwithcode <vladwithcode@gmail.com>
2022-06-07 15:45:51 -0500

    Bar Toggler for non menu pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/Layout/Header';
 import Sidebar from './components/Layout/Sidebar';
 import Toast from './components/Toast/Toast';
@@ -8,9 +8,11 @@ import SidebarContext from './context/Sidebar/SidebarContext';
 import CartService from './services/CartService';
 
 function App() {
+  const location = useLocation();
   const { isActive, setIsActive, contentId } = useContext(SidebarContext);
   const { setCart } = useContext(CartContext);
   const [cartInitialized, setCartInitialized] = useState(false);
+  const [isMenuPage, setIsMenuPage] = useState(false);
 
   useEffect(() => {
     if (!cartInitialized) {
@@ -25,16 +27,27 @@ function App() {
     }
   }, [cartInitialized]);
 
+  useEffect(() => {
+    if (location.pathname === '/') setIsMenuPage(true);
+    else setIsMenuPage(false);
+  }, [location]);
+
   return (
     <>
       <Header />
 
       <div
-        className={'header__toggler'.concat(isActive ? ' active' : '')}
+        className={'header__toggler'
+          .concat(!isMenuPage ? ' header__toggler--bar-toggler' : '')
+          .concat(isActive ? ' active' : '')}
         onClick={() => setIsActive(!isActive)}>
-        <span className='text'>FILTROS</span>
         <span className='bar bar--1'></span>
-        <span className='bar bar--2'></span>
+        {isMenuPage ? (
+          <span className='text'>FILTROS</span>
+        ) : (
+          <span className='bar bar--2'></span>
+        )}
+        <span className='bar bar--3'></span>
       </div>
 
       <Outlet />
