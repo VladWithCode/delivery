6db435ffab80c93c8cab69ccfc57b24cadd8e93b
vladwithcode <vladwithcode@gmail.com>
2022-06-13 17:40:23 -0500

    Admin Routes

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from '../App';
 import Checkout from '../components/Checkout/Checkout';
+import Dashboard from '../components/Dashboard/Dashboard';
+import Signin from '../components/Dashboard/Signin';
 import Home from '../components/Home/Home';
 import NotFound from '../components/NotFound/NotFound';
 import Order from '../components/Order/Order';
@@ -15,6 +17,10 @@ function AppRouter() {
           <Route path='/checkout' element={<Checkout />} />
           <Route path='/orden/:id' element={<Order />} />
           <Route path='*' element={<NotFound />} />
+        </Route>
+        <Route path='/admin/sign-in' element={<Signin />} />
+        <Route path='/admin' element={<Dashboard />}>
+          {/* <Route index element={}></Route> */}
         </Route>
       </Routes>
     </BrowserRouter>
