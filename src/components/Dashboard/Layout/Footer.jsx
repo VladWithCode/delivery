64346fb72316569cafbf849bf2b9902c0126ef53
vladwithcode <vladwithcode@gmail.com>
2022-06-13 17:29:05 -0500

    Dashboard Footer

---
+++ src/components/Dashboard/Layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className='dashboard__footer footer'>
+      <div className='footer__header'>
+        <p className='footer__heading'>SU TACO</p>
+      </div>
+      <p className='footer__copy fs-6 text-center'>
+        Made by <strong>GG Marketing</strong> &trade;
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
