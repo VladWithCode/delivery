0e5520c884b10b43dcff3120e43544d53a02f5fc
vladwithcode <vladwithcode@gmail.com>
2022-06-13 17:29:13 -0500

    Dashboard Header

---
+++ src/components/Dashboard/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='dashboard__header'>
+      <h2 className='h-2'>Dashboard</h2>
+    </div>
+  );
+}
+
+export default Header;
