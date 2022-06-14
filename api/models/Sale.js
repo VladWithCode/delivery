0cfbd5e2c349a1704a462500501f0f7bed59dc2f
vladwithcode <vladwithcode@gmail.com>
2022-06-14 16:51:13 -0500

    Default Timestamps

--- api/models/Sale.js
+++ api/models/Sale.js
@@ -1,55 +1,58 @@
 const { model, Schema, SchemaTypes } = require('mongoose');
 
-const SaleSchema = new Schema({
-  customer: new Schema(
-    {
-      id: String,
-      name: String,
-      phone: String,
-      email: String,
-    },
-    { _id: false }
-  ),
-  address: {
-    type: String,
-    required: true,
-  },
-  zip: String,
-  items: [
-    {
-      product: { type: SchemaTypes.ObjectId, ref: 'Product' },
-      sku: String,
-      name: String,
-      qty: Number,
-      unitPrice: Number,
-      total: Number,
-    },
-  ],
-  payment: new Schema(
-    {
-      method: { type: String, enum: ['card', 'cash', 'paypal'] },
-      isPayed: { type: Boolean, default: false },
-      payedOn: Date,
-      subtotal: Number,
-      tax: Number,
-      shipment: Number,
-      total: Number,
-    },
-    { _id: false }
-  ),
-  delivered: { type: Boolean, default: false },
-  deliveredAt: { type: Date },
-  stripeData: {
-    type: new Schema(
+const SaleSchema = new Schema(
+  {
+    customer: new Schema(
       {
-        intentId: { type: String, required: true },
-        customerId: String,
-        paymentMethodId: String,
+        id: String,
+        name: String,
+        phone: String,
+        email: String,
       },
       { _id: false }
     ),
-    required: false,
+    address: {
+      type: String,
+      required: true,
+    },
+    zip: String,
+    items: [
+      {
+        product: { type: SchemaTypes.ObjectId, ref: 'Product' },
+        sku: String,
+        name: String,
+        qty: Number,
+        unitPrice: Number,
+        total: Number,
+      },
+    ],
+    payment: new Schema(
+      {
+        method: { type: String, enum: ['card', 'cash', 'paypal'] },
+        isPayed: { type: Boolean, default: false },
+        payedOn: Date,
+        subtotal: Number,
+        tax: Number,
+        shipment: Number,
+        total: Number,
+      },
+      { _id: false }
+    ),
+    delivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+    stripeData: {
+      type: new Schema(
+        {
+          intentId: { type: String, required: true },
+          customerId: String,
+          paymentMethodId: String,
+        },
+        { _id: false }
+      ),
+      required: false,
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = model('Sale', SaleSchema);
