66c0eaa45d853dc0fd03a0b501c302eee8d3fa17
vladwithcode <vladwithcode@gmail.com>
2022-06-15 17:49:25 -0500

    Context Actions

--- src/context/Dashboard/DashboardProvider.js
+++ src/context/Dashboard/DashboardProvider.js
@@ -3,18 +3,32 @@ import { useToast } from '../../hooks/useToast';
 import SaleService from '../../services/SaleService';
 import DashboardContext from './DashboardContext';
 import DashboardReducer from './DashboardReducer';
-import { SET_ACTIVE_CTGS, SET_ACTIVE_PRICE_RANGES, SET_MENU } from './types';
+import { SET_CURRENT_ORDER, SET_ORDERS, UPDATE_ORDER } from './types';
 
 export const DashboardProvider = ({ children }) => {
-  const { displayErrorToast, displaySuccessToast } = useToast();
+  const { displayErrorToast } = useToast();
   const initialState = {
     orders: [],
-    currentOrder: {},
+    currentOrder: null,
   };
 
   const [state, dispatch] = useReducer(DashboardReducer, initialState);
 
-  const setOrders = orders => {};
+  const setOrders = orders => {
+    dispatch({ type: SET_ORDERS, payload: orders });
+  };
+
+  const setCurrentOrder = (id, forceOrder = null) => {
+    const order = forceOrder
+      ? forceOrder
+      : state.orders.find(o => o._id === id);
+
+    dispatch({ type: SET_CURRENT_ORDER, payload: order });
+  };
+
+  const updateOrder = (id, order) => {
+    dispatch({ type: UPDATE_ORDER, payload: { id, order } });
+  };
 
   useEffect(() => {
     const getOrders = async () => {
@@ -23,7 +37,11 @@ export const DashboardProvider = ({ children }) => {
       if (res.failed) {
         displayErrorToast(res.toastMessage);
       }
+
+      setOrders(res.sales);
     };
+
+    getOrders();
   }, []);
 
   return (
@@ -31,6 +49,9 @@ export const DashboardProvider = ({ children }) => {
       value={{
         orders: state.orders,
         currentOrder: state.currentOrder,
+        setOrders,
+        setCurrentOrder,
+        updateOrder,
       }}>
       {children}
     </DashboardContext.Provider>
