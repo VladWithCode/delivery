8498c78f4330c7444f36f9525a0056662b213d0e
vladwithcode <vladwithcode@gmail.com>
2022-06-15 15:14:49 -0500

    Return mongoose Sale and auto nanoid

--- api/services/sale.service.js
+++ api/services/sale.service.js
@@ -1,9 +1,11 @@
 const Sale = require('../models/Sale');
+const { nanoid } = require('nanoid');
 
 class SaleService {
   createSale(saleData) {
     const sale = new Sale({
       ...saleData,
+      _id: nanoid(10),
       customer: {
         name: saleData.customer.name,
         phone: saleData.customer.phone,
@@ -19,7 +21,7 @@ class SaleService {
   }
 
   async getSale(id) {
-    return await Sale.findOne({ _id: id }).lean();
+    return await Sale.findOne({ _id: id });
   }
 
   async saveSale(sale) {
