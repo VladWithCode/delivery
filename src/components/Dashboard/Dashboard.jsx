99864534dc0f462b4190b7e6a9676b479c515b28
vladwithcode <vladwithcode@gmail.com>
2022-06-15 17:49:42 -0500

    Detailed Order Modal

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React, { useContext } from 'react';
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
+import DashboardContext from '../../context/Dashboard/DashboardContext';
 import SidebarContext from '../../context/Sidebar/SidebarContext';
 import { useAuth } from '../../hooks/useAuth';
+import { isEmptyObject } from '../../utils/helpers';
 import Toast from '../Toast/Toast';
+import DetailedOrder from './DetailedOrder';
 import Header from './Layout/Header';
 import Sidebar from './Layout/Sidebar';
 
@@ -11,12 +15,20 @@ function Dashboard() {
   const navigate = useNavigate();
   const { isActive, setIsActive } = useContext(SidebarContext);
   const { isAdmin, init } = useAuth();
+  const { currentOrder } = useContext(DashboardContext);
+  const [modalActive, setModalActive] = useState(false);
 
   useEffect(() => {
     if (init && !isAdmin) {
       navigate('/admin/sign-in', { replace: true });
     }
   }, [isAdmin, init]);
+
+  useEffect(() => {
+    if (isEmptyObject(currentOrder)) return;
+
+    setModalActive(true);
+  }, [currentOrder]);
 
   return (
     <>
@@ -35,6 +47,7 @@ function Dashboard() {
       <Outlet />
       <Sidebar />
       <Toast />
+      <DetailedOrder active={modalActive} setActive={setModalActive} />
     </>
   );
 }
