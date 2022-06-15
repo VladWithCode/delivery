97d99d99067907403ca4c1f77596b468ebc7faee
vladwithcode <vladwithcode@gmail.com>
2022-06-15 17:48:08 -0500

    Header styles

--- src/components/Dashboard/Layout/Header.jsx
+++ src/components/Dashboard/Layout/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className='dashboard__header'>
-      <h2 className='h-2'>Dashboard</h2>
+    <div className='dashboard__header header'>
+      <h2 className='header__heading h-2'>Dashboard</h2>
     </div>
   );
 }
