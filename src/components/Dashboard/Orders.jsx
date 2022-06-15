55c080818b3f5f8b6ce5f64714d521224d8b96d4
vladwithcode <vladwithcode@gmail.com>
2022-06-15 17:50:59 -0500

    Lists for Pending & Finished orders

--- src/components/Dashboard/Orders.jsx
+++ src/components/Dashboard/Orders.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
+import Order from './Order';
+import OrderListing from './OrderListing';
 
 function Orders() {
   return (
-    <div className='dash-orders mt-5'>
-      <div className='dash-orders__header px-2 py-1'>
-        <h5 className='h5'>Ordenes</h5>
-        <button className='btn btn--info btn--right btn--sm'>Actualizar</button>
-      </div>
-      <div className='dash-orders__content px-2 py-1'>
-        <div className='dash-orders__listing'>
-          {/* <p className='dash-orders__no-content m-auto'>
-          No hay ordenes pendientes
-        </p> */}
-          <div className='dash-orders__item card card--dark card--noshadow mb-1'>
-            <div className='card__row fs-4 mb-0'>
-              <p className='name fw-500'>Jairo Rangel</p>
-              <p className='content text-mute m-auto'>6 productos</p>
-              <p className='price fw-600'>$1,500</p>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className='dash-orders py-6'>
+      <h1 className='fs-2 px-3 mb-2'>Ordenes</h1>
+      <OrderListing
+        title={'Ordenes Pendientes'}
+        mappingFn={(orders, handleOrderSelect) =>
+          orders
+            .filter(o => !o.delivered)
+            .map(o => (
+              <Order
+                id={o._id}
+                order={o}
+                handleOrderSelect={handleOrderSelect}
+                key={o._id}
+              />
+            ))
+        }
+      />
+      <OrderListing
+        title={'Ordenes Finalizadas'}
+        className='mt-2'
+        mappingFn={(orders, handleOrderSelect) =>
+          orders
+            .filter(o => o.delivered)
+            .map(o => (
+              <Order
+                id={o._id}
+                order={o}
+                handleOrderSelect={handleOrderSelect}
+                key={o._id}
+              />
+            ))
+        }
+      />
     </div>
   );
 }
