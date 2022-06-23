5bc4ccf147e325082a392a0edacb6947301309b2
vladwithcode <vladwithcode@gmail.com>
2022-06-23 16:53:12 -0500

    Map orders in-place

--- src/components/Dashboard/OrderListing.jsx
+++ src/components/Dashboard/OrderListing.jsx
@@ -29,24 +29,15 @@ function OrderListing({ title, className = '', mappingFn }) {
 
   useEffect(() => {
     if (mappingFn) {
-      setMappedOrders(mappingFn(orders, handleOrderSelect));
+      setMappedOrders(mappingFn(orders));
     } else {
-      setMappedOrders(
-        orders.map(o => (
-          <Order
-            id={o._id}
-            order={o}
-            handleOrderSelect={handleOrderSelect}
-            key={o._id}
-          />
-        ))
-      );
+      setMappedOrders(orders);
     }
   }, [orders, mappingFn]);
 
   return (
-    <div className={'order-card '.concat(className).trim()}>
-      <div className='order-card__header px-2 py-1'>
+    <div className={`dash-card ${className}`.trim()}>
+      <div className='dash-card__header px-2 py-1'>
         <h5 className='h5'>{title || 'Ordenes'}</h5>
         <button
           className='btn btn--info btn--right btn--sm'
@@ -54,13 +45,20 @@ function OrderListing({ title, className = '', mappingFn }) {
           Actualizar
         </button>
       </div>
-      <div className='order-card__content px-2 py-1'>
-        <div className='order-card__listing'>
+      <div className='dash-card__content px-2 py-1'>
+        <div className='dash-card__listing'>
           {mappedOrders.length > 0 ? (
-            mappedOrders
+            mappedOrders.map(o => (
+              <Order
+                id={o._id}
+                order={o}
+                handleOrderSelect={handleOrderSelect}
+                key={o._id}
+              />
+            ))
           ) : (
-            <p className='order-card__no-content my-1 fw-600'>
-              No hay ordenes pendientes
+            <p className='dash-card__no-content my-1 fw-600'>
+              No hay ordenes en esta categoria
             </p>
           )}
         </div>
